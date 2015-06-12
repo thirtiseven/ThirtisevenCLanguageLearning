b1bd24476062129d0d74c864ae7f37fe0bb3ea00
thirtiseven lee <ntlihy@gmail.com>
2015-06-13 07:57:43 +0800

    add the answer

---
+++ K&R/1-2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	printf("当printf函数的参数中包含\c");
+	return 0;
+}
+//Experiment to find out what happens when printf 's argument string contains \c, where c is some character not listed above.
