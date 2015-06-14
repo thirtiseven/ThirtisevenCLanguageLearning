077e014f037c8ca68af77105ba7a7fcc2c30b419
thirtiseven lee <ntlihy@gmail.com>
2015-06-14 15:45:43 +0800

    add

---
+++ K&R/1-6.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) 
+{
+	printf("Press a key. ENTER would be nice :-)\n\n");
+ 	printf("The expression getchar() != EOF evaluates to %d\n", 	getchar() != EOF); return 0;
+}
